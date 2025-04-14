f7f6a5c8514ce1900602f01ec1a38ed744e82486
bengris32 <bengris32@protonmail.ch>
2025-04-14 18:43:26 +0200

    rosemary: Kill CarrierConfigOverlay
    * Only seems to cause problems, and we don't really need it as
      we have force enabled IMS features again in commit e5018c6.
    
    Change-Id: Id8ffe30aee1d57ee4b4d61828b605f9da33f1d8c
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- device.mk
+++ device.mk
@@ -326,7 +326,6 @@ PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
     $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_PACKAGES += \
-    CarrierConfigOverlayRosemary \
     FrameworksResOverlayRosemary \
     SettingsProviderOverlayRosemary \
     SystemUIOverlayRosemary \
