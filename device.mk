2530ad2c9bf234327af903a57662c0ff719d1d0e
miui56 <payush706@gmail.com>
2025-06-08 10:02:47 +0700

    rosemary: Move to Xiaomi fingerprint AIDL

--- device.mk
+++ device.mk
@@ -157,8 +157,8 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.3-service.xiaomi
-
+    android.hardware.biometrics.fingerprint-service.xiaomi
+    
 PRODUCT_PACKAGES += \
     libvendor.goodix.hardware.biometrics.fingerprint@2.1.vendor
 
