af83452629bd99f9307260aa84168d1752eaac5d
Sarthak Roy <sarthakroy2002@gmail.com>
2025-07-18 10:55:36 +0200

    rosemary: Update VT from rothko A16
    Signed-off-by: Sarthak Roy <sarthakroy2002@gmail.com>
    Change-Id: I0b3dd9dc015d5eaa36847890fe6adc3be17374ab

--- extract-files.py
+++ extract-files.py
@@ -36,11 +36,8 @@ lib_fixups: lib_fixups_user_type = {
 }
 
 blob_fixups: blob_fixups_user_type = {
-    'system_ext/lib64/libsource.so': blob_fixup()
-        .add_needed('libui_shim.so'),
-    'system_ext/lib64/libsink-mtk.so': blob_fixup()
-        .replace_needed('libsensorndkbridge.so', 'android.hardware.sensors@1.0-convert-shared.so')
-        .add_needed('libshim_sink.so'),
+    'system_ext/lib64/libimsma.so': blob_fixup()
+        .replace_needed('libsink.so', 'libsink-mtk.so'),
     'system/priv-app/ImsService/ImsService.apk': blob_fixup()
         .apktool_patch('blob-patches/ImsService.patch', '-r'),
     ('vendor/bin/hw/android.hardware.gnss-service.mediatek', 'vendor/lib64/hw/android.hardware.gnss-impl-mediatek.so'): blob_fixup()
