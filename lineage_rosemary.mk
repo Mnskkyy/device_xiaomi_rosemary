f2fedb2d14252fa45287fd6cbe706fcfce6adc7f
probaero <87478728+probaero@users.noreply.github.com>
2025-12-07 20:53:49 +0700

    Revert "rosemary: Adapt to Halcyon"
    This reverts commit c11068c3fabf576807d575ba2e0c2516cabb7a27.

--- lineage_rosemary.mk
+++ lineage_rosemary.mk
@@ -11,10 +11,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from device makefile.
 $(call inherit-product, device/xiaomi/rosemary/device.mk)
 
-# Inherit some common Halcyon stuff.
-$(call inherit-product, vendor/halcyon/config/common.mk)
+# Inherit some common LineageOS stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-PRODUCT_NAME := halcyon_rosemary
+PRODUCT_NAME := lineage_rosemary
 PRODUCT_DEVICE := rosemary
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Redmi
